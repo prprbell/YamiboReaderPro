44781fbca14de136d1a6eea3d095bffc826cfa2f
prprbell <1727588371@qq.com>
2026-03-13 16:58:21 +0800

    v1.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.shirakawatyu.yamibo.novel"
         minSdk = 24
         targetSdk = 34
-        versionCode = 29
-        versionName = "1.5.1"
+        versionCode = 30
+        versionName = "1.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
