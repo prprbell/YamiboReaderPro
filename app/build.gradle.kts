047650076746423546a1b6112ec6d7068eb8ce14
prprbell <1727588371@qq.com>
2026-04-20 14:49:21 +0800

    refactor(gesture): 优化缩放平移手势处理器实现

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 80
-        versionName = "1.9.8"
+        versionName = "1.9.8-final"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
