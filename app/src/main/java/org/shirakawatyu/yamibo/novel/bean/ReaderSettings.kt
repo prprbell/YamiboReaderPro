c3b292114e05808dc82e7c162e26115ee2e69508
prprbell <1727588371@qq.com>
2026-05-26 17:28:22 +0800

    Fix reader mode settings serialization

--- app/src/main/java/org/shirakawatyu/yamibo/novel/bean/ReaderSettings.kt
+++ app/src/main/java/org/shirakawatyu/yamibo/novel/bean/ReaderSettings.kt
@@ -1,13 +1,33 @@
 package org.shirakawatyu.yamibo.novel.bean
 
-data class ReaderSettings(
+import com.alibaba.fastjson2.annotation.JSONCreator
+import com.alibaba.fastjson2.annotation.JSONField
+
+data class ReaderSettings @JSONCreator constructor(
+    @JSONField(name = "fontSizePx")
     var fontSizePx: Float? = null,
+
+    @JSONField(name = "lineHeightPx")
     var lineHeightPx: Float? = null,
+
+    @JSONField(name = "paddingDp")
     var paddingDp: Float? = null,
+
+    @JSONField(name = "nightMode")
     var nightMode: Boolean? = null,
+
+    @JSONField(name = "backgroundColor")
     var backgroundColor: String? = null,
+
+    @JSONField(name = "loadImages")
     var loadImages: Boolean? = null,
+
+    @JSONField(name = "isVerticalMode", alternateNames = ["verticalMode"])
     var isVerticalMode: Boolean? = null,
+
+    @JSONField(name = "translationMode")
     var translationMode: Int? = null,
+
+    @JSONField(name = "fontFamily")
     var fontFamily: Int? = null
 )
